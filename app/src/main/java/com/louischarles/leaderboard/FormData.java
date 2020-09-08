9840076d1383e450c73d6a5f9779df3bbf7b61a7
louischarles2000 <https://github.com/louischarles2000>
2020-09-08 17:16:43 +0300

    Fix the submission issue

--- app/src/main/java/com/louischarles/leaderboard/FormData.java
+++ app/src/main/java/com/louischarles/leaderboard/FormData.java
@@ -1,10 +1,12 @@
 package com.louischarles.leaderboard;
 
 public class FormData {
-//     Email Address = entry.1824927963
+
+// - Email Address = entry.1824927963
 //- Name = entry.1877115667
 //- Last Name = entry.2006916086
 //- Link to project = entry.284483984
+
     private String firstName;
     private String lastName;
     private String projectLink;
