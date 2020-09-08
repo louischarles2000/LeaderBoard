b85b1547489921a0ee803b023b4af9b3d5a5fc70
louischarles2000 <https://github.com/louischarles2000>
2020-09-08 17:20:41 +0300

    Re-commit

--- app/src/main/java/com/louischarles/leaderboard/ApiClient.java
+++ app/src/main/java/com/louischarles/leaderboard/ApiClient.java
@@ -10,6 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiClient {
+
     public static final String BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse/";
 
     public static Retrofit getRetrofit(){
